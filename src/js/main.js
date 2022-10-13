3186432f2515f69b299f6a3659c74d12b20fc0b4
lime7 <senseless.v.7@gmail.com>
2022-10-13 19:18:58 +0300

    Grid

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,50 @@ var slider = tns({
   items: 3,
   slideBy: 'page',
   autoplay: false,
+  nav: false,
+  prevButton: document.getElementById('#prev'),
+  nextButton: document.getElementById('#next'),
+  controlsText: ['<span class="fas fa-chevron-left"></span>', '<span class="fas fa-chevron-right"></span>'],
 })
 
 import { fslightbox } from 'fslightbox'
+
+
+
+// function navbarFixed(){
+//     if($('.page-header').length){
+//         $(window).scroll(function(){
+//             var scroll = $(window).scrollTop();
+
+//             if (scroll > 100 ){
+//                 $('.page-header').addClass("sticky");
+//             } else {
+//                 $('.page-header').removeClass("sticky");
+//             }
+//         });
+//     }
+// }
+// navbarFixed();
+
+
+// $('.form-control').on('focusin', function() {
+//     var 
+//         $this = $(this),
+//         row = $this.closest('.form-group'),
+//         label = row.find('label');
+
+//     row.addClass('active');
+// });
+
+// $('.form-control').on('focusout', function() {
+//     var 
+//         $this = $(this),
+//         row = $this.closest('.form-group'),
+//         label = row.find('label'),
+//         val = this.value;
+
+
+//     if (!val) {
+//         row.removeClass('active');
+//     }
+// });
